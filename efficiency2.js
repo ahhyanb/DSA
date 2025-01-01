bc24465253dc13f1b46900d438d9309a2e957c3f
Ryan Bartolome <rahhyahh@icloud.com>
2024-12-31 16:58:35 -1000

    clear

---
+++ efficiency2.js
@@ -0,0 +1,25 @@
+function sumIntegers2(n) {
+    return (n * (n + 1)) / 2;
+  }
+
+
+  const NUMBER_OF_REPETITIONS = 10; // Number of times to repeat the test
+const N = 100000;
+
+let sumOfRunningTime = 0n;
+
+for (let k = 1; k <= NUMBER_OF_REPETITIONS; k++) {
+  const start = process.hrtime.bigint();
+
+  const answer = sumIntegers2(N);
+
+  const end = process.hrtime.bigint();
+
+  sumOfRunningTime += end - start;
+}
+
+const averageTime = sumOfRunningTime / BigInt(NUMBER_OF_REPETITIONS);
+
+console.log(
+  `Summing ${N} numbers took an average of  ${averageTime} nanoseconds`
+);
